e7a0313d0a34c2afa87c4d36245049fdcd4b2785
karthikgotrala <karthikg85@hotmail.com>
2019-02-03 02:03:47 -0600

    add file upload and list object functionality

--- src/app/ng-aws-s3-crud/fileupload/fileupload.component.ts
+++ src/app/ng-aws-s3-crud/fileupload/fileupload.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import * as AWS from 'aws-sdk';
+import { callbackify } from 'util';
+import { nextTick } from 'q';
 
 
 @Component({
@@ -9,7 +11,19 @@ import * as AWS from 'aws-sdk';
 })
 export class FileuploadComponent implements OnInit {
 
-  selectedFiles:FileList;
+  showSummary:boolean=false;
+  @ViewChild("fileInput") nativeFile;
+  s3url:string="";
+  bucketName:string="";
+  fileUploadStaus:string="";
+  fileName:string="";
+  s3response:any=null;
+  allfileslist:any[]=[];
+  listObjects=false;
+  listLoading:boolean=false;
+  fileuploading:boolean=false;
+
+
   constructor() { }
 
   ngOnInit() {
@@ -17,14 +31,28 @@ export class FileuploadComponent implements OnInit {
 
 
   //Event listners
+  resetFileUpload(){
+  this.s3url="";
+  this.bucketName="";
+  this.fileUploadStaus="";
+  this.fileName="";
+  this.showSummary=false;
+  this.listObjects=false;
+      this.allfileslist=[];
+  }
+  triggerFileUpload(){
+    this.nativeFile.nativeElement.click();
+  }
 
   selectFile(fileInput:any){
+    this.fileuploading=true;
     const AWSService = AWS;
-    const region = 'us-east-1';
-    const bucketName = 'kgotgit-07232018';
-    const IdentityPoolId = 'us-east-1:4b607663-1066-46c2-94a5-9244a8a492c3';
+    const region = '<your bucket region>';
+    const bucketName = '<bucket Name>';
+    const IdentityPoolId = '<identity pool id>';
     const file = fileInput.target.files[0];
-  
+    this.fileName=file.name;
+    
   //Configures the AWS service and initial authorization
     AWSService.config.update({
       region: region,
@@ -42,15 +70,48 @@ export class FileuploadComponent implements OnInit {
   
   //I store this in a variable for retrieval later
   
-  
-    s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'public-read'}, function (err, data) {
-     if (err) {
+    this.showSummary=true;
+   
+    s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'private'}, (err, data)=> {
+      this.fileuploading=false;
+      if (err) {
        console.log(err, 'there was an error uploading your file');
+     }else{
+       this.s3response=data;
+      
      }
    });
+  
   }
-  upload(){
 
+  listAllFiles(){
+    this.listLoading=true;
+    const AWSService = AWS;
+    const region = '<your bucket region>';
+    const bucketName = '<bucket Name>';
+    const IdentityPoolId = '<identity pool id>';
+
+    //Configures the AWS service and initial authorization
+    AWSService.config.update({
+      region: region,
+      credentials: new AWSService.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      }),
+      
+    });
+    //adds the S3 service, make sure the api version and bucket are correct
+    const s3 = new AWSService.S3({
+      apiVersion: '2006-03-01',
+      params: { Bucket: bucketName}
+    });
+
+    s3.listObjects({Bucket:bucketName},(err,data)=>{
+      this.listObjects=true;
+      this.allfileslist=data.Contents;
+      console.log(data.Contents);
+      this.listLoading=false;
+    });
   }
+  
 
 }
